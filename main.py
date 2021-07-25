7bc2b10c74e7e10a757088963a525ca56ea86192
mtahbib <77113506+mtahbib@users.noreply.github.com>
2021-07-25 21:33:32 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("Welcome to random username generator")
+cname = input("What is your fav charachter/hero?\n")
+pname= input("Your fav pet name\n")
+print("Your username  is "+ cname +"_"+ pname )
